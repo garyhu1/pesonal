be79d6af6446e895c383b28155fc3ea42e016abc
garyhu <942984941@qq.com>
2018-10-30 22:04:18 +0800

    添加RestTemplate功能和OKHttp3依赖

--- src/main/java/com/garyhu/PersonalApplication.java
+++ src/main/java/com/garyhu/PersonalApplication.java
@@ -2,15 +2,23 @@ package com.garyhu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author: garyhu
  * @since: 2018/10/26 0026
- * @decription:
+ * @decription: 1、打包成war包时需要集成SpringBootServletInitializer，实现configure方法
  */
 
 @SpringBootApplication
-public class PersonalApplication {
+public class PersonalApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(PersonalApplication.class);
+    }
+
     public static void main(String[] args)throws Exception{
         SpringApplication.run(PersonalApplication.class,args);
     }
